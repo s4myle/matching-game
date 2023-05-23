5411b6b49535f8377b80f37abc0eef02f42dbb26
Samyle Carvalho <s4myle@gmail.com>
2023-05-23 16:25:59 -0400

    ✨ Create ArrowDown component

---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+function ArrowDown() { 
+    return /*html*/ `    
+        <img class='arrow1 -switch' src='../../images/arrow-down.png' alt='Arrow down icon'>
+    `
+};
+
+export default ArrowDown;
