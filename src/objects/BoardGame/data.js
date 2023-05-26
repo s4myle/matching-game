df753abac7c6ccf45d4585a986e8e8905d3f72c2
Samyle Carvalho <s4myle@gmail.com>
2023-05-25 21:21:57 -0400

    ✨ Create JS, HTML and CSS cards

---
+++ src/objects/BoardGame/data.js
@@ -0,0 +1,26 @@
+const cards = [{
+    icon: "html",
+    altIcon: "Logo do html"
+},
+{
+    icon: "html",
+    altIcon: "Logo do html"
+},
+{
+    icon: "javascript",
+    altIcon: "Logo do JavaScript"
+},
+{
+    icon: "javascript",
+    altIcon: "Logo do JavaScript"
+},
+{
+    icon: "css",
+    altIcon: "Logo do CSS"
+},
+{
+    icon: "css",
+    altIcon: "Logo do CSS"
+}];
+
+export default cards;
